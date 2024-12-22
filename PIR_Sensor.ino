40643e4a54bd30814966ad6bdd809a3d9c378275
Aviyan's Thoughts <93308974+AviyansThoughts@users.noreply.github.com>
2024-12-22 17:28:56 +0530

    Code

---
+++ PIR_Sensor.ino
@@ -0,0 +1,22 @@
+// Welcome to Aviyan's Thoughts
+
+int LED = D0;  // Digital pin D0
+int pir_sensor = D6;  // Digital pin D6
+void setup() {
+  Serial.begin(9600);
+  pinMode(pir_sensor, INPUT);   // declare sensor as input
+  pinMode(LED, OUTPUT);  // declare LED as output
+}
+void loop() {
+  long state = digitalRead(pir_sensor);
+    if(state == HIGH) {
+      digitalWrite (LED, HIGH);
+      Serial.println("Motion detected!");
+      delay(1000);
+    }
+    else {
+      digitalWrite (LED, LOW);
+      Serial.println("Motion not detected!");
+      delay(1000);
+      }
+}
